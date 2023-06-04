22855175800b51d77b1b88546dc5e888d6f5659e
IonutGrosu <ionutflorentin.grosu@gmail.com>
2023-06-04 14:24:46 +0200

    Initial Commit

---
+++ nCrypt/nCryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  nCryptApp.swift
+//  nCrypt
+//
+//  Created by Ionut Grosu on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct nCryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
